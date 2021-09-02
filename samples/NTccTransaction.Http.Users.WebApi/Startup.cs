2d48539eef1741371e212c65cbb733e183c69127
钟垒 <2141576@qq.com>
2021-09-02 10:51:14 +0800

    提交代码

--- samples/NTccTransaction.Http.Users.WebApi/Startup.cs
+++ samples/NTccTransaction.Http.Users.WebApi/Startup.cs
@@ -66,7 +66,7 @@ namespace NTccTransaction.Http.Users.WebApi
             containerBuilder.Register(Services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddLog4Net();
